634d62df1303c046681689ce37854e96f2fcec48
op06072 <op06072@gmail.com>
2025-02-02 05:32:38 +0900

    Update: update libaom version to 3

--- Package.swift
+++ Package.swift
@@ -22,10 +22,37 @@ let package = Package(
             name: "libaom",
             dependencies: ["libvmaf"],
             path: ".",
-            exclude: ["aom/av1/common/x86", "aom/av1/encoder/x86", "aom/aom_dsp/x86", "aom/aom_dsp/mips", "aom/aom_dsp/arm", "aom/aom_dsp/simd", "aom/aom_scale/mips", "aom/av1/common/arm", "aom/av1/common/ppc", "aom/av1/encoder/mips", "aom/av1/encoder/arm", "aom/aom_dsp/ssim.h", "aom/aom_dsp/ssim.c", "aom/aom_ports/ppc.h", "aom/aom_ports/ppc_cpudetect.c", "aom/aom_ports/arm.h", "aom/aom_ports/arm_cpudetect.c", "aom/av1/decoder/inspection.h", "aom/av1/decoder/inspection.c"],
-            sources: ["aom/aom", "aom/aom_dsp", "aom/aom_mem", "aom/aom_ports", "aom/aom_scale", "aom/aom_util", "aom/av1", "aom/third_party/vector", "aom/third_party/fastfeat", "generate", "aom/common/args_helper.h", "aom/common/args_helper.c"],
+            exclude: [
+                "aom/av1/common/x86", "aom/av1/common/ppc", "aom/av1/common/riscv",
+                "aom/av1/encoder/x86", "aom/aom_dsp/x86", "aom/aom_dsp/mips",
+                "aom/aom_scale/mips", "aom/av1/encoder/mips", "aom/aom_dsp/ssim.h",
+                "aom/aom_dsp/ssim.c", "aom/aom_ports/ppc.h", "aom/aom_ports/ppc_cpudetect.c",
+                "aom/aom_ports/riscv.h", "aom/aom_ports/riscv_cpudetect.c", "aom/av1/decoder/inspection.h",
+                "aom/av1/decoder/inspection.c", "aom/av1/encoder/tune_butteraugli.c",
+                "aom/av1/encoder/tune_butteraugli.h", "aom/av1/encoder/arm/av1_error_sve.c",
+                "aom/av1/encoder/arm/highbd_pickrst_sve.c", "aom/av1/encoder/arm/pickrst_sve.c",
+                "aom/av1/encoder/arm/pickrst_sve.h", "aom/av1/encoder/arm/wedge_utils_sve.c",
+                "aom/av1/common/arm/convolve_sve2.c", "aom/av1/common/arm/highbd_compound_convolve_sve2.c",
+                "aom/av1/common/arm/highbd_convolve_sve2.c", "aom/av1/common/arm/highbd_convolve_sve2.h",
+                "aom/av1/common/arm/highbd_warp_plane_sve.c", "aom/av1/common/arm/warp_plane_sve.c",
+                "aom/aom_dsp/arm/aom_neon_sve_bridge.h", "aom/aom_dsp/arm/aom_neon_sve2_bridge.h",
+                "aom/aom_dsp/arm/avg_sve.c", "aom/aom_dsp/arm/blk_sse_sum_sve.c",
+                "aom/aom_dsp/arm/highbd_convolve8_sve.c", "aom/aom_dsp/arm/highbd_sse_sve.c",
+                "aom/aom_dsp/arm/highbd_variance_sve.c", "aom/aom_dsp/arm/sum_squares_sve.c",
+                "aom/aom_dsp/flow_estimation/arm/disflow_sve.c", "aom/aom_dsp/flow_estimation/x86",
+                "aom/aom_dsp/butteraugli.c", "generate/CMakeFiles", "generate/gen_src"
+            ],
+            sources: [
+                "aom/aom", "aom/aom_dsp", "aom/aom_mem", "aom/aom_ports", "aom/aom_scale",
+                "aom/aom_util", "aom/av1", "aom/third_party/vector", "aom/third_party/fastfeat",
+                "generate", "aom/common/args_helper.h", "aom/common/args_helper.c"
+            ],
             publicHeadersPath: "include",
-            cSettings: [.headerSearchPath("generate"), .headerSearchPath("aom")]
+            cSettings: [
+                .headerSearchPath("generate"),
+                .headerSearchPath("aom"),
+                .unsafeFlags(["-march=armv8.3-a+dotprod+i8mm+simd"]),
+            ]
         )
     ],
     cLanguageStandard: .gnu11,
--- generate/config/aom_config.c
+++ generate/config/aom_config.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Alliance for Open Media. All rights reserved
+ * Copyright (c) 2025, Alliance for Open Media. All rights reserved.
  *
  * This source code is subject to the terms of the BSD 2 Clause License and
  * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
@@ -9,5 +9,5 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 #include "aom/aom_codec.h"
-static const char* const cfg = "cmake ../ -G \"Unix Makefiles\" -DAOM_TARGET_CPU=generic -DCONFIG_AV1_HIGHBITDEPTH=1 -DCONFIG_AV1_TEMPORAL_DENOISING=1 -DCONFIG_TUNE_VMAF=1";
+static const char* const cfg = "cmake ../aom -G \"Unix Makefiles\" -DCMAKE_TOOLCHAIN_FILE=\"../aom/build/cmake/toolchains/arm64-ios.cmake\" -DCONFIG_AV1_HIGHBITDEPTH=1 -DCONFIG_AV1_TEMPORAL_DENOISING=1 -DCONFIG_MULTITHREAD=1 -DCONFIG_INTERNAL_STATS=1 -DCONFIG_TUNE_BUTTERAUGLI=0 -DCONFIG_TUNE_VMAF=1 -DCONFIG_SALIENCY_MAP=1 -DENABLE_CCACHE=0 -DENABLE_TESTS=0";
 const char *aom_codec_build_config(void) {return cfg;}
